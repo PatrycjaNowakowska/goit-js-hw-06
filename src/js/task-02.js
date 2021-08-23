9566bae4c475fb3faa2692de8e8414e0584ca214
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-08-23 17:15:46 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector("ul");
+console.log(ul);
+
+ingredients.forEach(ingredient => {
+  // console.log(ingredient);
+  let item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  ul.append(item);
+});
+
+console.log(ul);
