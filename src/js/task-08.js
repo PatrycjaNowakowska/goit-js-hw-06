571909e51bbbf6249b6ea11efe2a9f145836a643
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-08-29 21:54:28 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+let form = document.querySelector("form");
 
+
+function handleSubmit(e) {
+    e.preventDefault();
+    
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Uzupełnij wszystkie pola!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
