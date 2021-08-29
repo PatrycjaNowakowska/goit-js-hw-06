cb72339727cd03a49513464980efd83de4941aae
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-08-29 20:23:52 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let body = document.querySelector("body");
+// console.log(body)
+let text = document.querySelector(".color");
+let btnChange = document.querySelector(".change-color");
+
+btnChange.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+}) 
